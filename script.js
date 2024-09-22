652f9a9a7f4ed5163861c6357762112a96b35e77
nizamgalleria <nizamgalleria@gmail.com>
2024-09-23 03:37:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function showPage(pageId) {
-  // Hide all sections
-  const sections = document.querySelectorAll('section');
-  sections.forEach(section => section.style.display = 'none');
 
-  // Show the selected section
-  document.getElementById(pageId).style.display = 'block';
-}
